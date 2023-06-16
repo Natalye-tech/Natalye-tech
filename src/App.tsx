da81c06bdd50811212e1beec8fc2b54a65dc16c4
Павловская Наталия Сергеевна <NPavlovskaya@inno.local>
2023-06-16 13:36:01 +0300

    Проект с запросом к listObjectItems?workflow_id=1

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
-import * as React from 'react';
+import React, { useState, useEffect } from 'react';
 
 export default function App() {
+  const [textObject, setTextObject] = useState('');
+
+  function getObjects() {
+  // fetch('http://localhost:8080/listObjectItems?workflow_id=1')
+  fetch('/listObjectItems?workflow_id=1')
+    .then(response => response.json())
+    .then(commits => setTextObject(JSON.stringify(commits)))
+    .catch(error => {
+       setTextObject(error.toString());
+     })
+  }
+
+  getObjects();
+
+  useEffect(() => {
+    console.log(" textObject = ", textObject);
+  }, [ textObject, setTextObject ]);
+
   return (
     <>
-    Test React, TypeScript +++++++++++
+    <div><b>Объекты:</b></div>
+    {textObject}
     </>
   );
 }
